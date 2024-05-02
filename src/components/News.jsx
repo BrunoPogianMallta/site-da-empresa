a73e151d975155a64500955ce3fb8193e1505be6
unknown <pggian1985@gmail.com>
2024-05-02 19:42:34 -0300

    add:  estrutura do site

---
+++ src/components/News.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News = () => {
+  return (
+    <div>
+      <h2>Blog</h2>
+      {/* Listagem de artigos do blog */}
+    </div>
+  );
+}
+
+export default News;
---
+++ src/components/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <div>
+      <h2>Solicite um Orçamento</h2>
+      {/* Formulário de solicitação de orçamento */}
+    </div>
+  );
+}
+
+export default Quote;
